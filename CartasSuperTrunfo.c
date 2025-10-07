e2636d30c6f2255bf4113bf5bcac2bdf6d09ca5f
DiogoprogramaC <diogo.mendes.17@hotmail.com>
2025-10-07 19:39:52 +0000

    Criei seugunda Cartas SuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -46,11 +46,53 @@ printf("Estado: %s\n", Estado_Carta1);
 printf("Codigo: %s\n", Codigo_Carta1);
 printf("Nome da Cidade: %s\n", Cidade_Carta1);
 printf("Populacao: %d\n", Populacao_Carta1);
-printf("Area em Km²: %F\n", Area_Carta1);
+printf("Area em Km²: %f\n", Area_Carta1);
 printf("Numero do Pib: %f\n", Pib_Carta1);
 printf("Numeros de Pontos Turisticos: %d\n", PontoTuristico_Carta1);
 
+// Dados da Primeira Carta---
 
+char Estado_Carta2 [20];
+char Codigo_Carta2[4];
+char Cidade_Carta2[50];
+int Populacao_Carta2;
+float Area_Carta2;
+float Pib_Carta2;
+int PontoTuristico_Carta2;
 
+printf("---Cadastro da Segunda Carta ---\n");
 
-} 
+printf("Digite o Estado:");
+scanf("%s", Estado_Carta2);
+
+printf("Digite o codigo da Carta:");
+scanf("%s", Codigo_Carta2);
+
+printf("Digite o nome da Cidade:");
+scanf("%s", Cidade_Carta2);
+
+printf("Digite a Populacao:");
+scanf("%d", &Populacao_Carta2);
+
+printf("Digite a Area KM²:");
+scanf("%f", &Area_Carta2);
+
+printf("Digite o Pib:");
+scanf("%f", &Pib_Carta2);
+
+printf("Digite o ponto Turistico:");
+scanf("%d", &PontoTuristico_Carta2);
+
+// ---Saida de dados da segunda Carta ---
+
+printf("Estado: %s\n", Estado_Carta2);
+printf("Codigo: %s\n", Codigo_Carta2);
+printf("Nome da cidade: %s\n", Cidade_Carta2);
+printf("Populacao: %d\n", Populacao_Carta2);
+printf("Area em Km²: %f\n", Area_Carta2);
+printf("Numero do Pib: %f\n", Pib_Carta2);
+printf("Numero de Pontos Turisticos: %d\n", PontoTuristico_Carta2);
+
+return 0;
+
+}
